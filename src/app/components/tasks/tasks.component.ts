114bcc74b9d0221efc668103bd23bf2d41c3decf
Piyush Garg <piyushgargofficial@gmail.com>
2022-09-19 15:13:01 -0400

    CRUD Added

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../../services/task.service';
+import {Task} from '../../Tasks'
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+
+  tasks: Task[] = [];
+
+  //to use the service, we need to pass it as the argument in the constructor.
+  constructor(private taskService: TaskService) { }
+
+  ngOnInit(): void {
+
+    //similar to promises, you get the promise and then we do a .then()
+    this.taskService.getTasks().subscribe((tasks)=> (this.tasks = tasks));
+
+  }
+
+  deleteTask(task: Task){
+    this.taskService.deleteTask(task).subscribe(()=> (this.tasks = this.tasks.filter((t) => t.id !== task.id)));
+  }
+
+  toggleReminder(task: Task){
+    task.reminder = !task.reminder
+    this.taskService.updateTaskReminder(task).subscribe()
+  } 
+
+  addTask(task: Task){
+    this.taskService.addTask(task).subscribe((task)=> (this.tasks.push(task)));
+  }
+}
